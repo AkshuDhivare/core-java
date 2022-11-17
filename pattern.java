298182f4f9585991cebfa3f9e42e55712a9d9245
Akshay Dhivare <47208883+AkshuDhivare@users.noreply.github.com>
2022-11-18 02:40:11 +0530

    Update pattern.java
    output : -
    *
    **
    ***
    ****
    *****
    ****
    ***
    **
    *

--- pattern.java
+++ pattern.java
@@ -1,7 +1,7 @@
-public class pattern {
+public class pattern1 {
 
 	public static void main(String[] args) {
-		// normal pattern
+		
 		for (int i = 1; i <= 5; i++) {
 			for (int j = 1; j <= i; j++) {
 				System.out.print(" * ");
