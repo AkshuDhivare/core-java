7b714dbc3933a28886d606c97b96c8b5cc43de5a
Akshay Dhivare <47208883+AkshuDhivare@users.noreply.github.com>
2022-11-18 02:41:56 +0530

    Update pattern.java

--- pattern.java
+++ pattern.java
@@ -1,7 +1,5 @@
 public class pattern1 {
-
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) {	
 		for (int i = 1; i <= 5; i++) {
 			for (int j = 1; j <= i; j++) {
 				System.out.print(" * ");
@@ -16,5 +14,4 @@ public class pattern1 {
 			System.out.println();
 		}
 	}
-
 }
